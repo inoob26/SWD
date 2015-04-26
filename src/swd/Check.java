29bbed0673216a52d17e51f337743969d198a06c
inoob <inoob@192.168.1.102>
2015-04-26 12:43:33 +0300

    Fixed regular expression

--- src/swd/Check.java
+++ src/swd/Check.java
@@ -5,19 +5,29 @@ import java.util.regex.Matcher;
 public class Check {
     //Проверка формы добавления фирмы
     public boolean check_TextfieldFirmTitle(String string){
-        Pattern p = Pattern.compile("^\\w+(\\s|\\'|\\\")?(\\w+)?(\\1)?$");
+        Pattern p = Pattern.compile("^([a-z]|[A-Z]|[а-я]|[А-Я]|\\s|\\'|\\\"|[0-9])+$");
+        //"^([a-z]|[A-Z]|[а-я]|[А-Я]|\\s)+$"
+        //"^\\w+(\\s|\\'|\\\")?(\\w+)?(\\1)?$"
         Matcher m = p.matcher(string);
         return m.matches();
     }
     
     public boolean check_TextfieldPhone(String string){
-        Pattern p = Pattern.compile("^\\d+$");
+        Pattern p = Pattern.compile("^(\\d|-)+$");
+        Matcher m = p.matcher(string);
+        return m.matches();
+    }
+    
+    public boolean check_TextfieldContract(String string){
+        Pattern p = Pattern.compile("^([a-z]|[A-Z]|[а-я]|[А-Я]|[0-9])+$");
         Matcher m = p.matcher(string);
         return m.matches();
     }
     
     public boolean check_TextfieldAddress(String string){
-        Pattern p = Pattern.compile("^(\\w+||[\\\"\\'-]|\\s)+$");
+        Pattern p = Pattern.compile("^(\\w|[а-я]|[А-Я]|[0-9]|\\.|\\s|\\\"|\\'|\\-)+$");
+        //"^(\\w|[а-я]|[А-Я]|[0-9]|\\.|\\s|\\\"|\\'|\\-)+$"
+        //"^(\\w+||[\\\"\\'-]|\\s)+$"
         Matcher m = p.matcher(string);
         return m.matches();
     }
